73cc944e7b4489e537fd557fc7cf2208ba95da84
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2022-11-11 09:42:50 +0500

    solved issue

--- 2-masala/Program.cs
+++ 2-masala/Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace _2_masala
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("num1 = ");
+            string num1 = Console.ReadLine();
+            Console.Write("num2 = ");
+            string num2 = Console.ReadLine();
 
+            if (num1.Length > num2.Length)
+            {
+                while(num2.Length != num1.Length)
+                {
+                    num2 = '0' + num2;
+                }
+            }
+            else
+            {
+                while (num2.Length != num1.Length)
+                {
+                    num1 = '0' + num1;
+                }
+            }
+
+            Console.WriteLine(" " + num1);
+            Console.WriteLine('+');
+            Console.WriteLine(" " + num2);
+            string sum = "";
+            int saver = 0;
+
+            for(int i = num1.Length - 1; i >= 0; i--)
+            {
+                sum = ((num1[i] + num2[i] - 96) % 10 + saver).ToString() + sum;
+                saver = (num1[i] + num2[i] - 96) / 10;
+            }
+            if (saver == 1)
+                sum = saver + sum;
+            Console.WriteLine(" " + sum);
         }
     }
 }
